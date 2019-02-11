77931791540cc646e8f840071dc644bc9a2ef175
efistokl <mykola@pentonet.com>
2019-02-11 13:29:57 +0200

    ext-config.js: generate configuration for neighbor cells: for cell reselection

--- configs/ext-config.js
+++ configs/ext-config.js
@@ -15,7 +15,40 @@ function parse(value, type) {
   return value
 }
 
-function getConfiguration(serialNumber, callback) {
+function makeSingleNeighbourConfiguration(commonConfiguration, neighbourCellConfiguration, neighbourIndex) {
+    const pre = `Device.Services.FAPService.1.CellConfig.UMTS.RAN.NeighborList.IntraFreqCell.${neighbourIndex}`
+    let result = {}
+
+    const getField = key => neighbourCellConfiguration.hasOwnProperty(key) ? neighbourCellConfiguration[key] : commonConfiguration[key]
+
+    const [lac, rac] = getField('Device.Services.FAPService.1.CellConfig.UMTS.CN.LACRAC').split(/:|\|/)
+
+    result[`${pre}.CID`] = getField('Device.Services.FAPService.1.CellConfig.UMTS.RAN.CellID')
+    result[`${pre}.LAC`] = `${lac}|xsd:unsignedInt`
+    result[`${pre}.RAC`] = `${rac}|xsd:unsignedInt`
+    result[`${pre}.RNCID`] = getField('Device.Services.FAPService.1.CellConfig.UMTS.RAN.RNCID')
+    result[`${pre}.PCPICHScramblingCode`] = getField('Device.Services.FAPService.1.CellConfig.UMTS.RAN.RF.PrimaryScramblingCode').split('|')[0]+ '|xsd:unsignedInt'
+    result[`${pre}.PLMNID`] = getField('Device.Services.FAPService.1.CellConfig.UMTS.CN.PLMNID')
+    result[`${pre}.X_000295_TxDiversityIndicator`] = '1|xsd:unsignedInt'
+
+    return result
+}
+
+function getNeigboursConfiguration(config, serialNumber) {
+    neighbours = Object
+        .keys(config)
+        .filter(key => key !== 'Common' && key !== serialNumber)
+        .map(key => config[key])
+        .map((neighbourCellConfiguration, i) => {
+            const index = i + 1
+            return makeSingleNeighbourConfiguration(config['Common'], neighbourCellConfiguration, index)
+        })
+
+    return Object.assign({}, ...neighbours)
+}
+
+function getConfiguration(args, callback) {
+  const serialNumber = args[0]
   try {
     const config = ini.parse(fs.readFileSync(configFile, 'utf-8'))
 
@@ -23,7 +56,9 @@ function getConfiguration(serialNumber, callback) {
 
     const specificConfiguration = config[serialNumber] || {}
 
-    const mergedConfiguration = Object.assign(commonConfiguration, specificConfiguration)
+    const neighboursConfiguration = getNeigboursConfiguration(config, serialNumber)
+
+    const mergedConfiguration = Object.assign(commonConfiguration, specificConfiguration, neighboursConfiguration)
 
     const result = Object.keys(mergedConfiguration).map(propertyName => {
       const [value, type] = mergedConfiguration[propertyName].split('|')
